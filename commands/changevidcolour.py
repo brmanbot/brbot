15f57dfd4646390e4acd27d763771208b6e58f70
brmanbot <132795936+brmanbot@users.noreply.github.com>
2023-05-12 14:57:47 +0100

    Updated config.py
    Increased cooldown to 7 days

--- commands/changevidcolour.py
+++ commands/changevidcolour.py
@@ -10,28 +10,28 @@ video_manager = None
     description="Change the colour of a video in the database.",
     guild_ids=GUILD_IDS,
     options=[
-        disnake.Option("name", "The name of the video to change colour.", type=disnake.OptionType.string, required=True, autocomplete=True),
+        disnake.Option("identifier", "The name or URL of the video to change colour.", type=disnake.OptionType.string, required=True, autocomplete=True),
         disnake.Option("colour", "The new colour for the video (green, red, or yellow).", type=disnake.OptionType.string, required=True, autocomplete=True)
     ]
 )
-async def changevidcolour(inter: disnake.ApplicationCommandInteraction, name: str, colour: str):
+async def changevidcolour(inter: disnake.ApplicationCommandInteraction, identifier: str, colour: str):
     valid_colours = ["green", "red", "yellow"]
-    colour = colour.lower()
-    if colour not in valid_colours:
+    if colour.lower() not in valid_colours:
         await inter.response.send_message("Invalid colour. Please use `green`, `red`, or `yellow`.")
         return
 
-    old_colour = await video_manager.get_video_color(name)
+    identifier_type = "url" if identifier.startswith("https://tinyurl.com/") else "name"
+    old_colour = await video_manager.get_video_color(identifier, identifier_type)
     if old_colour is None:
-        await inter.response.send_message(f"No video found with name `{name}`")
+        await inter.response.send_message(f"No video found with identifier `{identifier}`")
         return
 
     if old_colour == colour:
-        await inter.response.send_message(f"The video `{name}` is already in the `{colour}` database.")
+        await inter.response.send_message(f"The video `{identifier}` is already in the `{colour}` database.")
         return
 
-    await video_manager.change_video_color(name, colour)
-    await inter.response.send_message(f"Moved `{name}` from `{old_colour}` to `{colour}` database.")
+    await video_manager.change_video_color(identifier, identifier_type, colour)
+    await inter.response.send_message(f"Moved `{identifier}` from `{old_colour}` to `{colour}` database.")
 
 
 @changevidcolour.autocomplete("colour")
@@ -39,6 +39,6 @@ async def changevidcolour_autocomplete(inter: ApplicationCommandInteraction, use
     return await autocomp_colours(inter, user_input)
 
 
-@changevidcolour.autocomplete("name")
-async def changevidcolour_autocomplete_name(inter: ApplicationCommandInteraction, user_input: str):
+@changevidcolour.autocomplete("identifier")
+async def changevidcolour_autocomplete_identifier(inter: ApplicationCommandInteraction, user_input: str):
     return await autocomp_video_names(inter, user_input)
