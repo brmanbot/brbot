d828a0cf9f671c586aadeed180dd0f26525c37b6
brmanbot <132795936+brmanbot@users.noreply.github.com>
2023-11-06 00:34:50 +0000

    Update thread_monitor.py

--- commands/thread_monitor.py
+++ commands/thread_monitor.py
@@ -5,19 +5,17 @@ import asyncio
 class ThreadMonitor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.TARGET_CHANNEL_ID = 1100169484939038881
+        self.TARGET_CHANNEL_ID = 1082813041122496602
 
     @commands.Cog.listener()
     async def on_thread_create(self, thread):
         if thread.parent_id == self.TARGET_CHANNEL_ID:
-            # Wait a bit for messages to possibly appear in the thread
-            await asyncio.sleep(2)  # wait for 1 second
+            await asyncio.sleep(2)
             messages = await thread.history(limit=100).flatten()
             if not messages:
                 print("No messages to repost.")
                 return
 
-            # Collect messages to repost
             repost_messages = []
             for message in messages[::-1]:
                 content = message.content
@@ -26,17 +24,14 @@ class ThreadMonitor(commands.Cog):
                 if message.embeds:
                     content += ' ' + ' '.join(str(embed.to_dict()) for embed in message.embeds)
 
-                # Format the message
                 formatted_message = f"{message.author.mention}: {content}\n"
                 if len(formatted_message) <= 2000:
                     repost_messages.append(formatted_message)
                 else:
-                    # Split long message into multiple messages
                     repost_messages.extend([formatted_message[i:i+2000] for i in range(0, len(formatted_message), 2000)])
 
             await thread.delete()
 
-            # Send the collected messages
             parent_channel = self.bot.get_channel(self.TARGET_CHANNEL_ID)
             if parent_channel:
                 for repost_message in repost_messages:
