f3a31b8ebbfcf29a55347d8cb2f786864211cc88
brmanbot <132795936+brmanbot@users.noreply.github.com>
2023-11-06 00:06:45 +0000

    Update thread_monitor.py

--- commands/thread_monitor.py
+++ commands/thread_monitor.py
@@ -1,6 +1,10 @@
-from disnake.ext import commands
 import disnake
+from disnake.ext import commands
 from datetime import timedelta
+import logging
+
+# Set up basic logging to print to stderr
+logging.basicConfig(level=logging.INFO)
 
 class ThreadMonitor(commands.Cog):
     def __init__(self, bot):
@@ -9,33 +13,42 @@ class ThreadMonitor(commands.Cog):
 
     @commands.Cog.listener()
     async def on_thread_create(self, thread):
-        if thread.parent_id == self.TARGET_CHANNEL_ID:
-            await disnake.utils.sleep_until(thread.created_at + timedelta(seconds=1))
-            messages = await thread.history(limit=100).flatten()
-            if not messages:
-                print("No messages to repost.")
-                return
+        try:
+            if thread.parent_id == self.TARGET_CHANNEL_ID:
+                await disnake.utils.sleep_until(thread.created_at + timedelta(seconds=1))
+                messages = await thread.history(limit=100).flatten()
+                
+                # Debugging: Print messages
+                logging.info(f"Fetched {len(messages)} messages from the thread.")
 
-            await thread.delete()
+                # Temporarily comment out deletion for testing
+                # await thread.delete()
 
-            parent_channel = self.bot.get_channel(self.TARGET_CHANNEL_ID)
-            if parent_channel:
-                repost_message = "Messages reposted from a thread:\n"
-                for message in messages[::-1]:
-                    repost_message += f"{message.author.mention}: {message.content}\n"
-                    if len(repost_message) > 1900:
-                        try:
+                parent_channel = self.bot.get_channel(self.TARGET_CHANNEL_ID)
+                if parent_channel:
+                    repost_message = "Messages reposted from a thread:\n"
+                    for message in messages[::-1]:
+                        repost_message += f"{message.author.mention}: {message.content}\n"
+                        # Check if the message length is over the Discord limit and send
+                        if len(repost_message) > 1900:
                             await parent_channel.send(repost_message)
-                        except Exception as e:
-                            print(f"Failed to send message: {e}")
-                        repost_message = ""
-                if repost_message:
-                    try:
+                            repost_message = ""
+                    # Send any remaining content
+                    if repost_message:
                         await parent_channel.send(repost_message)
-                    except Exception as e:
-                        print(f"Failed to send message: {e}")
-            else:
-                print(f"Could not find the parent channel with ID {self.TARGET_CHANNEL_ID}")
+                else:
+                    logging.error(f"Could not find the parent channel with ID {self.TARGET_CHANNEL_ID}")
+        except Exception as e:
+            logging.exception("An error occurred in on_thread_create:")
+    
+    # Manual command to test functionality
+    @commands.command()
+    async def testrepost(self, ctx, thread_id: int):
+        thread = ctx.guild.get_thread(thread_id)
+        if thread:
+            messages = await thread.history(limit=100).flatten()
+            for message in messages:
+                await ctx.send(f"{message.author.mention}: {message.content}")
 
 def setup(bot):
     bot.add_cog(ThreadMonitor(bot))
