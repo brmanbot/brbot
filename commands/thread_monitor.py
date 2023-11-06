6119b40c3d0ca67da4f5c9ab5248e8ebf33c4838
brmanbot <132795936+brmanbot@users.noreply.github.com>
2023-11-06 00:32:10 +0000

    Update thread_monitor.py

--- commands/thread_monitor.py
+++ commands/thread_monitor.py
@@ -1,5 +1,6 @@
 from disnake.ext import commands
 import disnake
+import asyncio
 
 class ThreadMonitor(commands.Cog):
     def __init__(self, bot):
@@ -9,36 +10,36 @@ class ThreadMonitor(commands.Cog):
     @commands.Cog.listener()
     async def on_thread_create(self, thread):
         if thread.parent_id == self.TARGET_CHANNEL_ID:
+            # Wait a bit for messages to possibly appear in the thread
+            await asyncio.sleep(2)  # wait for 1 second
             messages = await thread.history(limit=100).flatten()
             if not messages:
                 print("No messages to repost.")
                 return
 
+            # Collect messages to repost
+            repost_messages = []
+            for message in messages[::-1]:
+                content = message.content
+                if message.attachments:
+                    content += ' ' + ' '.join(attachment.url for attachment in message.attachments)
+                if message.embeds:
+                    content += ' ' + ' '.join(str(embed.to_dict()) for embed in message.embeds)
+
+                # Format the message
+                formatted_message = f"{message.author.mention}: {content}\n"
+                if len(formatted_message) <= 2000:
+                    repost_messages.append(formatted_message)
+                else:
+                    # Split long message into multiple messages
+                    repost_messages.extend([formatted_message[i:i+2000] for i in range(0, len(formatted_message), 2000)])
+
             await thread.delete()
 
+            # Send the collected messages
             parent_channel = self.bot.get_channel(self.TARGET_CHANNEL_ID)
             if parent_channel:
-                repost_message = ""
-                for message in messages[::-1]:
-                    content = message.content
-                    if message.attachments:
-                        content += '\n'.join(attachment.url for attachment in message.attachments)
-                    if message.embeds:
-                        content += '\n'.join(str(embed.to_dict()) for embed in message.embeds)
-
-                    new_message = f"{message.author.mention}: {content}\n"
-                    if len(repost_message) + len(new_message) > 1900:
-                        # Send current message if the new message would exceed the limit
-                        try:
-                            await parent_channel.send(repost_message)
-                        except Exception as e:
-                            print(f"Failed to send message: {e}")
-                        repost_message = new_message  # Start a new message with the new content
-                    else:
-                        repost_message += new_message
-
-                # After loop, send any remaining content
-                if repost_message:
+                for repost_message in repost_messages:
                     try:
                         await parent_channel.send(repost_message)
                     except Exception as e:
