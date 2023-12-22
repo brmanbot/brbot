81b927cfb3add67e8eca80bee5cdf10ff55b32de
brmanbot <132795936+brmanbot@users.noreply.github.com>
2023-12-22 00:34:20 +0000

    gallery update and remove shorten for now

--- commands/tiktok.py
+++ commands/tiktok.py
@@ -1,21 +1,27 @@
 import disnake
 from disnake.ext import commands
 import aiohttp
-from utils import shorten_url
 
 async def resolve_short_url(url):
     async with aiohttp.ClientSession() as session:
         async with session.head(url, allow_redirects=True) as response:
             return str(response.url)
 
+async def fetch_tiktok_content(session, url):
+    async with session.post("https://api.tik.fail/api/grab", headers={"User-Agent": "MyTikTokBot"}, data={"url": url}) as response:
+        if response.status == 200:
+            return await response.json()
+        else:
+            return None
+
 def setup(bot):
     @bot.slash_command(
         name="tiktok",
-        description="Provide a shortened direct link to a TikTok video.",
+        description="Provide links to TikTok video or gallery images.",
         options=[
             disnake.Option(
                 "url",
-                "Enter the URL of the TikTok video.",
+                "Enter the URL of the TikTok content.",
                 type=disnake.OptionType.string,
                 required=True
             )
@@ -30,21 +36,21 @@ def setup(bot):
             resolved_url = url
 
         async with aiohttp.ClientSession() as session:
-            async with session.post("https://api.tik.fail/api/grab", headers={"User-Agent": "MyTikTokBot"}, data={"url": resolved_url}) as response:
-                if response.status == 200:
-                    json_response = await response.json()
-                    if json_response.get("success") and "data" in json_response and "download" in json_response["data"]:
-                        video_link = json_response["data"]["download"]["video"].get("NoWM", {}).get("url")
-                        
-                        if video_link:
-                            shortened_link = await shorten_url(video_link)
-                            if shortened_link:
-                                await ctx.edit_original_response(content=f"{shortened_link}")
-                            else:
-                                await ctx.edit_original_response(content="Failed to shorten the video link.")
-                        else:
-                            await ctx.edit_original_response(content="No 'No Watermark' video quality found.")
+            tiktok_response = await fetch_tiktok_content(session, resolved_url)
+            if tiktok_response and tiktok_response.get("success"):
+                # Handle gallery (list of image URLs)
+                if isinstance(tiktok_response["data"].get("download"), list):
+                    image_links = tiktok_response["data"]["download"]
+                    formatted_links = [f"[Image {index + 1}]({link})" for index, link in enumerate(image_links)]
+                    await ctx.edit_original_response(content="\n".join(formatted_links))
+                # Handle video
+                elif "video" in tiktok_response["data"].get("download", {}):
+                    video_link = tiktok_response["data"]["download"]["video"].get("NoWM", {}).get("url")
+                    if video_link:
+                        await ctx.edit_original_response(content=f"[Video]({video_link})")
                     else:
-                        await ctx.edit_original_response(content="Failed to get video link from the response.")
+                        await ctx.edit_original_response(content="No video found.")
                 else:
-                    await ctx.edit_original_response(content=f"Error fetching video link. Status Code: {response.status}")
+                    await ctx.edit_original_response(content="Unsupported TikTok content.")
+            else:
+                await ctx.edit_original_response(content="Failed to fetch TikTok content.")
