a7313b17f79036f7755d6c7e5a85a914c8eb2aa8
brmanbot <132795936+brmanbot@users.noreply.github.com>
2024-01-10 17:39:22 +0000

    added pie chart

--- commands/updatecooldown.py
+++ commands/updatecooldown.py
@@ -1,6 +1,16 @@
+import json
 import disnake
-from config import GUILD_IDS, ALLOWED_USER_ID, update_cooldown
+import time
+import matplotlib.pyplot as plt
+import os
+import aiohttp
+import matplotlib.patheffects as pe
+from commands.showcooldown import format_cooldown_for_title
+from config import GUILD_IDS, ALLOWED_USER_ID, update_cooldown, get_cooldown
 from utils import bot
+
+IMAGE_PATH = 'cooldown_pie_chart_update.png'
+
 
 def setup(bot):
     @bot.slash_command(
@@ -44,4 +54,72 @@ def setup(bot):
             value_seconds = value
 
         update_cooldown(value_seconds)
-        await ctx.send(f"Cooldown updated to `{value} {unit}`")
+        cooldown_value = get_cooldown()
+        current_time = time.time()
+        on_cooldown = 0
+
+        with open('video_data.json', 'r') as file:
+            video_data = json.load(file)
+
+        all_videos = {video for color_list in video_data['video_lists'].values() for video in color_list}
+        total_videos = len(all_videos)
+
+        for video in all_videos:
+            last_played = video_data['played_videos'].get(video)
+            if last_played and (current_time - last_played < cooldown_value):
+                on_cooldown += 1
+
+        available_videos = total_videos - on_cooldown
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        fig.patch.set_visible(False)
+        ax.axis('off')
+
+        color_labels = ['On Cooldown', 'Available']
+        sizes = [on_cooldown, available_videos]
+        colors = ['#A40000', '#4E9A06']
+
+        _, _, autotexts = plt.pie(
+            sizes,
+            labels=None,
+            autopct='%1.1f%%',
+            colors=colors,
+            wedgeprops=dict(width=0.3),
+            pctdistance=0.85,
+            textprops={'fontsize': 24, 'color': 'white'}
+        )
+
+        plt.setp(
+            autotexts,
+            path_effects=[pe.withStroke(linewidth=3, foreground='black')]
+        )
+
+        plt.legend(
+            [f"{label} {size}" for label, size in zip(color_labels, sizes)],
+            loc="upper left",
+            bbox_to_anchor=(0, 1),
+            fontsize=14
+        )
+
+        plt.subplots_adjust(
+            left=0, bottom=0, right=1, top=1, wspace=0, hspace=0
+        )
+        plt.savefig(IMAGE_PATH, transparent=True)
+
+        async with aiohttp.ClientSession() as session:
+            with open(IMAGE_PATH, 'rb') as f:
+                async with session.post('https://0x0.st', data={'file': f}) as resp:
+                    if resp.status != 200:
+                        await ctx.send('Could not upload the image.')
+                        return
+                    else:
+                        img_url = await resp.text()
+
+        os.remove(IMAGE_PATH)
+
+        cooldown_for_title = format_cooldown_for_title(cooldown_value)
+        embed_title = f"Cooldown Updated: {cooldown_for_title}"
+        embed = disnake.Embed(title=embed_title, color=disnake.Color.blue())
+        embed.set_image(url=img_url)
+
+        await ctx.send(embed=embed)
