62e1eaee1265d93ca402aafcc67d63e037345fd8
brmanbot <132795936+brmanbot@users.noreply.github.com>
2024-02-27 11:23:36 +0000

    made it better

--- commands/vids.py
+++ commands/vids.py
@@ -28,11 +28,19 @@ def setup(bot):
 
     async def create_embed(colour, page, items_per_page=30):
         videos = await fetch_videos(colour)
+        video_links = ''
         total_pages = math.ceil(len(videos) / items_per_page)
         page = max(1, min(page, total_pages))
 
-        video_links = '\n'.join(
-            [f"[{video[0]}]({video[1]})" for video in videos[(page - 1) * items_per_page: page * items_per_page]])
+        while items_per_page > 0:
+            start_index = (page - 1) * items_per_page
+            end_index = start_index + items_per_page
+            video_links = '\n'.join(
+                [f"[{video[0]}]({video[1]})" for video in videos[start_index:end_index]])
+
+            if len(video_links) <= 4096:
+                break
+            items_per_page -= 1
 
         embed = disnake.Embed(
             title=f"{colour.capitalize()} videos ({len(videos)})",
@@ -40,7 +48,8 @@ def setup(bot):
             color=disnake.Color.green() if colour == "green" else (
                 disnake.Color.red() if colour == "red" else disnake.Color.gold())
         )
-        embed.set_footer(text=f"Page {page} of {total_pages}")
+        embed.set_footer(
+            text=f"Page {page} of {math.ceil(len(videos) / max(items_per_page, 1))}")
 
         return embed
 
@@ -57,42 +66,8 @@ def setup(bot):
             message = await self.ctx.original_message()
             await message.edit(view=self)
 
-        @disnake.ui.button(label="Green", style=ButtonStyle.green, custom_id="green_vids", row=0)
-        async def green_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            self.colour = "green"
-            self.page = 1
-            await self.update_embed(interaction)
-
-        @disnake.ui.button(label="Red", style=ButtonStyle.red, custom_id="red_vids", row=0)
-        async def red_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            self.colour = "red"
-            self.page = 1
-            await self.update_embed(interaction)
-
-        @disnake.ui.button(label="Yellow", style=ButtonStyle.blurple, custom_id="yellow_vids", row=0)
-        async def yellow_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            self.colour = "yellow"
-            self.page = 1
-            await self.update_embed(interaction)
-
-        @disnake.ui.button(label="Previous", style=ButtonStyle.secondary, custom_id="previous_page", row=1, disabled=True)
-        async def previous_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            self.page = max(1, self.page - 1)
-            await self.update_embed(interaction)
-
-        @disnake.ui.button(label="Next", style=ButtonStyle.secondary, custom_id="next_page", row=1)
-        async def next_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            self.page += 1
-            await self.update_embed(interaction)
-
-        @disnake.ui.button(label="Last Page", style=ButtonStyle.secondary, custom_id="last_page", row=1)
-        async def last_page_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-            embed = await create_embed(self.colour, self.page)
-            total_pages = int(embed.footer.text.split()[-1])
-            self.page = total_pages
-            await self.update_embed(interaction)
-
-        async def update_embed(self, interaction):
+        async def defer_and_update_embed(self, interaction):
+            await interaction.response.defer()
             embed = await create_embed(self.colour, self.page)
 
             if self.page == 1:
@@ -116,4 +91,39 @@ def setup(bot):
                 elif component.custom_id == "last_page":
                     self.children[idx] = self.last_page_button
 
-            await interaction.response.edit_message(embed=embed, view=self)
+            await interaction.edit_original_message(embed=embed, view=self)
+
+        @disnake.ui.button(label="Green", style=ButtonStyle.green, custom_id="green_vids", row=0)
+        async def green_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            self.colour = "green"
+            self.page = 1
+            await self.defer_and_update_embed(interaction)
+
+        @disnake.ui.button(label="Red", style=ButtonStyle.red, custom_id="red_vids", row=0)
+        async def red_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            self.colour = "red"
+            self.page = 1
+            await self.defer_and_update_embed(interaction)
+
+        @disnake.ui.button(label="Yellow", style=ButtonStyle.blurple, custom_id="yellow_vids", row=0)
+        async def yellow_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            self.colour = "yellow"
+            self.page = 1
+            await self.defer_and_update_embed(interaction)
+
+        @disnake.ui.button(label="Previous", style=ButtonStyle.secondary, custom_id="previous_page", row=1, disabled=True)
+        async def previous_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            self.page = max(1, self.page - 1)
+            await self.defer_and_update_embed(interaction)
+
+        @disnake.ui.button(label="Next", style=ButtonStyle.secondary, custom_id="next_page", row=1)
+        async def next_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            self.page += 1
+            await self.defer_and_update_embed(interaction)
+
+        @disnake.ui.button(label="Last Page", style=ButtonStyle.secondary, custom_id="last_page", row=1)
+        async def last_page_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+            embed = await create_embed(self.colour, self.page)
+            total_pages = int(embed.footer.text.split()[-1])
+            self.page = total_pages
+            await self.defer_and_update_embed(interaction)
