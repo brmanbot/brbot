fb97a53473348c1f3e7abd7adbbb1a3363e1368f
brmanbot <132795936+brmanbot@users.noreply.github.com>
2024-03-04 22:46:12 +0000

    funny fuzzy match update

--- commands/getvid.py
+++ commands/getvid.py
@@ -3,6 +3,7 @@ import disnake
 from disnake import ApplicationCommandInteraction, OptionChoice
 from utils import bot, autocomp_video_names, fetch_videos_by_name_or_hashtag
 from config import GUILD_IDS
+from rapidfuzz import process, fuzz
 
 
 def setup(bot):
@@ -36,27 +37,46 @@ def setup(bot):
 
     @getvid.autocomplete("identifier")
     async def getvid_autocomplete(inter: ApplicationCommandInteraction, user_input: str):
-        user_input_terms = user_input.lower().strip().split()
-        suggestions = []
+        user_input = user_input.lower().strip()
+        all_matches = []
 
-        is_hof_search = user_input.lower().strip() == "hof"
-
-        general_terms = [
-            term for term in user_input_terms if not term.startswith("#")]
-        hashtag_terms = [term[1:]
-                         for term in user_input_terms if term.startswith("#")]
+        exact_name_match_score = 10000
+        exact_hashtag_match_score = 7500
 
         for video_name, video_info in inter.bot.video_manager.videos_info.items():
+            if video_info is None:
+                continue
+
             name_lower = video_name.lower()
             hashtags = (video_info.get("hashtags") or "").lower().split(',')
-            hashtags_clean = [tag.strip()
-                              for tag in hashtags if tag.strip()]
+            hashtags_clean = [tag.strip() for tag in hashtags if tag.strip()]
 
+            if user_input == name_lower:
+                match_score = exact_name_match_score
+            elif user_input in hashtags_clean:
+                match_score = exact_hashtag_match_score
+            else:
+                name_score = process.extractOne(
+                    user_input, [name_lower], scorer=fuzz.WRatio)[1]
+                hashtag_scores = [process.extractOne(user_input, [tag], scorer=fuzz.WRatio)[
+                    1] for tag in hashtags_clean]
+                max_hashtag_score = max(hashtag_scores, default=0)
+                match_score = max(name_score, max_hashtag_score * 0.9)
+
+            if match_score > 60:
+                all_matches.append((video_name, video_info, match_score))
+
+        sorted_matches = sorted(
+            all_matches, key=lambda x: x[2], reverse=True)[:25]
+
+        suggestions = []
+        for video_name, video_info, _ in sorted_matches:
             is_hall_of_fame = video_info["is_hall_of_fame"]
             hof_emoji = "🏆" if is_hall_of_fame else ""
             added_by = video_info["added_by"].split('#')[0]
-            formatted_hashtags = ', '.join(
-                [f'#{tag}' for tag in hashtags_clean])
+            hashtags_clean = [f'#{tag}' for tag in (video_info.get(
+                "hashtags") or "").lower().split(',') if tag.strip()]
+            formatted_hashtags = ', '.join(hashtags_clean)
 
             suggestion_text = f"{hof_emoji}{video_name}".strip()
             if added_by:
@@ -64,14 +84,9 @@ def setup(bot):
             if formatted_hashtags:
                 suggestion_text += f" [{formatted_hashtags}]"
 
-            matches_hof_criteria = is_hof_search and is_hall_of_fame
-            general_match = any(term in name_lower or any(
-                term in hashtag for hashtag in hashtags_clean) for term in general_terms)
-            hashtag_match = all(any(hashtag_term in hashtag for hashtag in hashtags_clean)
-                                for hashtag_term in hashtag_terms)
+            suggestion_text = suggestion_text[:100]
 
-            if (matches_hof_criteria or general_match or not general_terms) and (hashtag_match or not hashtag_terms) and len(suggestions) < 25:
-                suggestions.append(disnake.OptionChoice(
-                    name=suggestion_text, value=video_name))
+            suggestions.append(disnake.OptionChoice(
+                name=suggestion_text, value=video_name))
 
         return suggestions
